a5e27e1bb0c60fa478d252f1f763112e6c8fc909
Viktor Orlyanskiy <orlyanskiy_va@mail.ru>
2023-08-07 23:20:20 +0300

    add botcheck url

--- utils/puppeteer-extra.js
+++ utils/puppeteer-extra.js
@@ -5,32 +5,35 @@ import { writeFile } from "./writeFile.js";
 
 puppeteer.use(StealthPlugin());
 
-const URL =
-    "https://sbermarket.ru/metro/kofe-zhokey-po-vostochnomu-molotyy-250-g-8f2dd1b";
+const URL = "https://bot.sannysoft.com/";
 const TIMEOUT = 4000;
 
-puppeteer.launch({ headless: true }).then(async (browser) => {
-    console.log("Running tests..");
+puppeteer
+    .launch({ headless: true, args: ["--no-sandbox"] })
+    .then(async (browser) => {
+        console.log("Running tests..");
 
-    const page = await browser.newPage({ arg: ["--window-size=1900x1080"] });
+        const page = await browser.newPage({
+            arg: ["--window-size=1900x1080"],
+        });
 
-    const context = browser.defaultBrowserContext();
-    await context.overridePermissions(URL, ["geolocation"]);
-    // set current location to Tokyo Station
-    const client = await page.target().createCDPSession();
-    await client.send("Emulation.setGeolocationOverride", {
-        latitude: 59.93,
-        longitude: 30.31,
-        accuracy: 100,
+        const context = browser.defaultBrowserContext();
+        await context.overridePermissions(URL, ["geolocation"]);
+        // set current location to Tokyo Station
+        const client = await page.target().createCDPSession();
+        await client.send("Emulation.setGeolocationOverride", {
+            latitude: 59.93,
+            longitude: 30.31,
+            accuracy: 100,
+        });
+
+        await page.goto(URL);
+        await page.waitForTimeout(TIMEOUT);
+
+        const content = await page.content();
+        writeFile("content", content);
+
+        await page.screenshot({ path: "testresult.png", fullPage: true });
+        await browser.close();
+        console.log(`All done, check the screenshot. ✨`);
     });
-
-    await page.goto(URL);
-    await page.waitForTimeout(TIMEOUT);
-
-    const content = await page.content();
-    writeFile("content", content);
-
-    await page.screenshot({ path: "testresult.png", fullPage: true });
-    await browser.close();
-    console.log(`All done, check the screenshot. ✨`);
-});
