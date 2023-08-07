ff95920b3f79aa88863235769e7c285b06bf93f7
Viktor Orlyanskiy <orlyanskiy_va@mail.ru>
2023-08-07 23:08:23 +0300

    add UserAgent

--- utils/puppeteer.js
+++ utils/puppeteer.js
@@ -1,7 +1,7 @@
 import puppeteer from "puppeteer-extra";
 import StealthPlugin from "puppeteer-extra-plugin-stealth";
 
-const LAUNCH_PUPPETEER_OPTIONS = { headless: true, args: ["--no-sandbox"] };
+const LAUNCH_PUPPETEER_OPTIONS = { headless: false, args: ["--no-sandbox"] };
 const PAGE_OPTIONS = { arg: ["--window-size=1900x1080"] };
 
 const URL = "https://www.auchan.ru/product/klubnika-zam-0-3kg/";
@@ -23,6 +23,10 @@ export const getPageContent = async (url) => {
             longitude: 30.31,
             accuracy: 100,
         });
+
+        await page.setUserAgent(
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+        );
 
         await page.goto(url);
         await page.waitForTimeout(TIMEOUT);
